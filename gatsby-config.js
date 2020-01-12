bd5a29933ed2ebbd47058f18835d42ea87bb6b42
Ryan Filler <ryanfiller89@gmail.com>
2020-01-12 11:44:43 -0600

    add src/images to gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,13 @@ module.exports = {
 
     // data related plugins
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `content`,
